ed56f9acff932ac413109d4dbe9138038f8139fd
Rafi Imam <rafiimam96@gmail.com>
2023-11-01 18:01:09 +0600

    all functionality added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import "./index.css"; // Make sure to import your CSS file
 
 const App = () => {
   const [media, setMedia] = useState(Media);
+  const [selectedItems, setSelectedItems] = useState([]);
   const [draggedIndex, setDraggedIndex] = useState(null);
+
+  const handleToggleSelect = (index) => {
+    const updatedSelectedItems = [...selectedItems];
+    if (updatedSelectedItems.includes(index)) {
+      updatedSelectedItems.splice(updatedSelectedItems.indexOf(index), 1);
+    } else {
+      updatedSelectedItems.push(index);
+    }
+    setSelectedItems(updatedSelectedItems);
+  };
+
+  const handleDeleteSelected = () => {
+    const updatedMedia = media.filter((_, index) => !selectedItems.includes(index));
+    setMedia(updatedMedia);
+    setSelectedItems([]);
+  };
 
   const handleDragStart = (index) => {
     setDraggedIndex(index);
@@ -32,14 +49,17 @@ const App = () => {
       <div className="media-container">
         {media.map((file, index) => (
           <div
+            key={index}
             className={`media ${index === 0 ? "first-image" : ""} ${
+              selectedItems.includes(index) ? "selected" : ""
+            } ${
               index === draggedIndex ? "dragged" : ""
             }`}
-            key={index}
             draggable
             onDragStart={() => handleDragStart(index)}
             onDragOver={handleDragOver(index)}
             onDragEnd={handleDragEnd}
+            onClick={() => handleToggleSelect(index)}
           >
             {file.type === "image" ? (
               <img src={file.url} alt="" />
@@ -49,6 +69,8 @@ const App = () => {
           </div>
         ))}
       </div>
+
+      <button className="btn" onClick={handleDeleteSelected}>Delete Selected</button>
     </div>
   );
 };
